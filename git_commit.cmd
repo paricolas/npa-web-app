405cf8c7bf7d748fe1504dbcd9e539f9687114c5
paricolas <paricolas@gmail.com>
2016-12-20 11:41:54 +0100

    2016122011:41:54.23-git bash file  change

--- git_commit.cmd
+++ git_commit.cmd
@@ -1,8 +1,7 @@
-
 cd c:/perso/github/npa-web-app
 git status
 git add * 
 set /p newComment=""
-IF "%newComment%" =="" (git commit -m "%DATE:~6,4%%DATE:~3,2%%DATE:~0,2%%time%")
-ELSE (git commit -m "%DATE:~6,4%%DATE:~3,2%%DATE:~0,2%%time%-%newComment%")
+if "%newComment%" =="" (git commit -m "%DATE:~6,4%%DATE:~3,2%%DATE:~0,2%%time%")
+if not "%newComment%" =="" (git commit -m "%DATE:~6,4%%DATE:~3,2%%DATE:~0,2%%time%-%newComment%")
 git push
