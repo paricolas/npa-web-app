776fc79bcd5963679f45a6082f5d511331e6a417
paricolas <paricolas@gmail.com>
2016-12-20 11:32:49 +0100

    2016122011:32:49.43

--- git_commit.cmd
+++ git_commit.cmd
@@ -1,5 +1,8 @@
+
 cd c:/perso/github/npa-web-app
 git status
-git add *
-git commit -m "%DATE:~6,4%%DATE:~3,2%%DATE:~0,2%%time%"
+git add * 
+set /p newComment=""
+IF "%newComment%" =="" (git commit -m "%DATE:~6,4%%DATE:~3,2%%DATE:~0,2%%time%")
+ELSE (git commit -m "%DATE:~6,4%%DATE:~3,2%%DATE:~0,2%%time%-%newComment%")
 git push
